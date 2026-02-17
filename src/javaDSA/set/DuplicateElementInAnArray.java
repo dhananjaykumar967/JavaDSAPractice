86e39a8d17bc417ac3251915f75cb856283aebd3
Dhananjay Kumar <dhananjay.kumar967@gmail.com>
2026-02-17 23:10:14 +0530

    Added DuplicateElementInAnArray

---
+++ src/javaDSA/set/DuplicateElementInAnArray.java
@@ -0,0 +1,13 @@
+package javaDSA.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateElementInAnArray {
+    public static void main(String[] args) {
+        String str = "abcbbaabcbbbcefaaf";
+        Set<Character> charSet = new HashSet<>();
+
+
+    }
+}
